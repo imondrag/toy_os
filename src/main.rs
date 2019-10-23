e9ec41d03c0a67ce2e0ae7d88fe82bf173f0f5ed
Ivan Mondragon <imondrag@umich.edu>
2019-10-23 18:35:07 -0400

    write hello world to vga buffer

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,18 @@
 // panic handler
 mod panic;
 
+static MSG: &[u8] = b"Hello, world!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buf = 0xb8000 as *mut u8;
+
+    for (i, &byte) in MSG.iter().enumerate() {
+        unsafe {
+            *vga_buf.add(2 * i) = byte;
+            *vga_buf.add(2 * i + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
