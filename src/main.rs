dbea4d04dcbf9fd0c9283f46f2e784e74f4df9bd
Ivan Mondragon <imondrag@umich.edu>
2019-10-23 19:28:54 -0400

    use u16

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ static MSG: &[u8] = b"Hello, world!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buf = 0xb8000 as *mut u8;
+    let vga_buf = 0xb8000 as *mut u16;
 
     for (i, &byte) in MSG.iter().enumerate() {
+        let attr: u16 = 0x0f00;
+        let colored_char: u16 = attr | byte as u16;
+
         unsafe {
-            *vga_buf.add(2 * i) = byte;
-            *vga_buf.add(2 * i + 1) = 0xb;
+            vga_buf.add(i).write(colored_char);
         }
     }
 
