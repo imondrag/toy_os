1b75613b394e3b58309c609ff72fbee4d52e39b1
Ivan Mondragon <imondrag@umich.edu>
2020-08-13 14:52:29 -0500

    big update

---
+++ src/lib.rs
@@ -0,0 +1,73 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
+// #![feature(alloc_error_handler)]
+#![feature(const_fn)]
+#![feature(const_in_array_repeat_expressions)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+// extern crate alloc;
+extern crate rlibc;
+
+pub mod panic;
+pub mod qemu;
+pub mod serial;
+pub mod vga;
+
+use qemu::{exit_qemu, QemuExitCode};
+
+pub fn init() {
+    // gdt::init();
+    // interrupts::init_idt();
+    // unsafe { interrupts::PICS.lock().initialize() };
+    // x86_64::instructions::interrupts::enable();
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+    init();
+    test_main();
+    hlt_loop();
+}
+
+// #[alloc_error_handler]
+// fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+//     panic!("allocation error: {:?}", layout)
+// }
--- src/panic.rs
+++ src/panic.rs
@@ -1,8 +1,21 @@
-// panic.rs: panic-halt -- halt the thread on panic; messages are discarded
-
+use crate::qemu::{exit_qemu, QemuExitCode};
 use core::panic::PanicInfo;
 
+pub fn test_panic_handler(_info: &PanicInfo) -> ! {
+    // serial_println!("[failed]\n");
+    // serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    // println!("{}", info);
+    crate::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
 }
